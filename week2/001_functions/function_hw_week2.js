42580160bcb65ed29631b7b6dddb76ed7504a815
Marie-Claire Balabanian <marieclaire.balabanian@gmail.com>
2016-06-09 04:34:13 -0500

    002_css_layouts Homework

---
+++ week2/001_functions/function_hw_week2.js
@@ -0,0 +1,33 @@
+
+
+
+
+    // CALCULATE AGE BASED ON BIRTH YEAR AND CURRENT YEAR
+
+var birthYr = prompt("What year were you born?");
+var getYr = new Date();
+var currentYr = getYr.getFullYear();
+
+function calculateAge(birthYr, currentYr) {
+  var age1 = currentYr - birthYr;
+  var age2 = currentYr - birthYr + 1;
+  return("You are either " + age1 + " or " + age2 + ".");
+}
+
+console.log(calculateAge(birthYr, currentYr));
+
+
+
+    // CALCULATE HOW MANY SNACKS YOU NEED TO LAST THE REST OF YOUR LIFE
+
+var age = prompt("How old are you now?")
+var death = prompt("How old do you think you'll be when you die?")
+var snack = prompt("What is your favorite snack?")
+var dailyServing = prompt("How many snacks do you eat per day")
+
+function calculateSupply(age, dailyServing) {
+  var helpings = (((death - age) * 365) * Math.round(dailyServing));
+  return "You will need " + helpings + " helpings of " + snack + " to last you until the ripe old age of " + death + ".";
+}
+
+console.log(calculateSupply(age, dailyServing));
--- week2/001_functions/main.js
+++ week2/001_functions/main.js
@@ -28,4 +28,39 @@ function calculateSupply (age, amountPerDay) {
 }
 calculateSupply(104, 2);
   
+
+
+
+// ------------------------------------------//
+
+
+function concatinate (string, number) {
+  var numberToString = number.toString();
+  return string + " " + numberToString;
+}
+  concatinate("hi", 4);
   
+  
+  
+  
+// ----------------------- //
+function concat (theArray) {
+  return theArray[0] + theArray[1] + theArray[2]
+}
+ 
+concat ([ 'hi', 'bye', 'yo']);
+
+
+or
+
+
+function concat (array) {
+  console.log(array.join(' '))
+}
+
+// ----------------------------------- //
+
+function concat (parentArray) {
+  return stuArray[0][0] + stuArray[0][1] + stuArray[0][2] + stuArray[1]
+
+}
