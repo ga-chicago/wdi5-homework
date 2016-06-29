4ae70edb7be5f097474ec2785589de122fb2bd9c
Stu Porter <sporter21@gmail.com>
2016-06-29 09:10:32 -0500

    tuesday

---
+++ week5/01_http/app.js
@@ -0,0 +1,10 @@
+
+
+var movie = require("./movie");
+
+
+
+var threeFavouriteMovies = ["pineapple express", "goodfellas", "goon"];
+threeFavouriteMovies.forEach(function(film){
+  movie.get(film);
+})
