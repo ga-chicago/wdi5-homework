4c8ead0cf1229d05d73e79e1ce4a11be0646ac0b
mrqrvly <mrqrvly@gmail.com>
2016-07-20 19:52:11 -0500

    getting ready to pull down for homework

--- week8/02_classes_objects_reps/marque_reavley/classes.rb
+++ week8/02_classes_objects_reps/marque_reavley/classes.rb
@@ -1,3 +1,60 @@
 #  CLASSES EXERCISE - WEEK 8, DAY 2
 #  ================================
 
+#  Require HTTParty
+#  ----------------
+require 'HTTParty'
+
+#  Set a class for movies
+#  ----------------------
+class Movies
+
+  # Initialize a new movie instance
+  # -------------------------------
+  def initialize(movie_title, movie_description, omdb_url)
+    @movie_title       = movie_title
+    @movie_description = movie_description
+    @omdb_url          = omdb_url
+  end
+
+  # Setter method for movie_title
+  # -----------------------------
+  def movie_title=(movie_title)
+    @movie_title = movie_title
+  end
+
+  # getter method for movie_title
+  # -----------------------------
+  def movie_title
+    @movie_title
+  end
+
+  # setter method for movie_description
+  # -----------------------------------
+  def movie_description=(movie_description)
+    @movie_description = movie_description
+  end
+
+  # getter method for movie_description
+  # -----------------------------------
+  def movie_description
+    @movie_description
+  end
+
+  # function to set url where we get the info
+  # -----------------------------------------
+  def set_omdb_url(url)
+    
+    # setter method for url
+    # ---------------------
+    def omdb_url=(omdb_url)
+      @omdb_url = omdb_url
+    end
+
+    # getter method for url
+    # ---------------------
+    def omdb_url
+      @omdb_url
+    end
+  end
+
